bd094fe3d1fd6bad39600a8295044eb3e1ab1057
Ramakant9621 <117703588+Ramakant9621@users.noreply.github.com>
2023-12-24 15:53:16 +0530

    Add files via upload

---
+++ Javascriptfundamental Assignment/Introduction to Javascript operations/Answer5.js
@@ -0,0 +1,9 @@
+let principalAmount = 5000;
+let interestRate = 10;
+let timePeriod = 2;
+let interest = (principalAmount * interestRate * timePeriod)/100;
+
+console.log("Principal Amount:", principalAmount);
+console.log("Interest Rate:", interestRate, "%");
+console.log("Time Period:", timePeriod, "years");
+console.log("Simple Interest:", interest);
---
+++ Javascriptfundamental Assignment/Introduction to Javascript operations/Answer6.js
@@ -0,0 +1,7 @@
+let weightInKG = 63;
+let heightInMeters = 1.75;
+let bmiResult = weightInKG/(heightInMeters*heightInMeters);
+
+console.log("Weight:", weightInKG, "kg");
+console.log("Height:", heightInMeters, "m");
+console.log("BMI:", bmiResult);
